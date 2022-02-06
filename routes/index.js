497355c72d09d92541e77c75d0586a63431ace2d
Tarun Verma <verma.tarun200109@gmail.com>
2022-02-06 11:34:49 +0530

    some changes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 //Routes
 
-router.use('/api/users', require('./userRoutes'));
-router.use('/api/locations', require('./locationRoutes'));
-router.use('/api/hotelTypes', require('./hotelTypeRoutes'));
+router.use('/api/user', require('./userRoutes'));
+router.use('/api/location', require('./locationRoutes'));
+router.use('/api/hotelType', require('./hotelTypeRoutes'));
 router.use('/api/hotel', require('./hotelRoutes'));
 router.use('/api/wishList', require('./wishListRoutes'));
 
